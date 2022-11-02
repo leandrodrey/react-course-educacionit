26dab6834dbe9a4aabd6cff1d9f54a872cb86a51
Leandro Rey <leandrodrey@gmail.com>
2022-11-02 18:10:02 -0300

    Fixes

--- mi-proyecto/src/CLASE2/Tarea/componenteContenido.js
+++ mi-proyecto/src/CLASE2/Tarea/componenteContenido.js
@@ -6,7 +6,8 @@ function ComponenteContenido() {
     return (
         <React.Fragment>
             <p className="estiloParrafo">También se añade la función Diccionario para niños, que permitirá al asistente
-                proporcionar respuestas más apropiadas y amigables</p>
+                proporcionar respuestas más apropiadas y amigables
+            </p>
         </React.Fragment>
     )
 }
